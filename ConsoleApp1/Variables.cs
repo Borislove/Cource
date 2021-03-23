a07e345b64ac87a8586fc2cec253ba0863b69d7b
Borislove <abirme@yandex.ru>
2021-03-23 09:39:46 +0300

    test

---
+++ ConsoleApp1/Variables.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Переменные в C#
+    Объявление (создание) переменных
+    Инициализация переменных
+    Присвоение значения переменной (помещение данных)
+
+ */
+namespace ConsoleApp1
+{
+    class Variables
+    {
+        static void Main(string[] args)
+        
+        {
+            //Тип_данных имя_переменной;
+            int a ; 
+            a = 5;
+        }
+        
+        
+
+                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                             
+
+
+
+
+
+
+            
+
+
+
+                                
+    }
+}
